0a5ece502c82fa7044474217d483843772bd4bc9
stephan-lopes <40838038+stephan-lopes@users.noreply.github.com>
2023-09-20 01:25:02 -0300

    feat: completed the exercise

--- coordinate-transformation.js
+++ coordinate-transformation.js
@@ -15,7 +15,9 @@
  *  translated coordinate pair in the form [x, y]
  */
 export function translate2d(dx, dy) {
-  throw new Error('Implement the translate2d function');
+  return function(x, y) {
+    return [x+dx, y+dy]
+  }
 }
 
 /**
@@ -29,7 +31,9 @@ export function translate2d(dx, dy) {
  *  scaled coordinate pair in the form [x, y]
  */
 export function scale2d(sx, sy) {
-  throw new Error('Implement the scale2d function');
+  return function(x, y) {
+    return [x*sx, y*sy]
+  }
 }
 
 /**
@@ -43,7 +47,9 @@ export function scale2d(sx, sy) {
  *  transformed coordinate pair in the form [x, y]
  */
 export function composeTransform(f, g) {
-  throw new Error('Implement the composeTransform function');
+  return function (x, y) {
+    return g(...f(x,y))
+  }
 }
 
 /**
@@ -56,5 +62,20 @@ export function composeTransform(f, g) {
  *  if the arguments are the same on subsequent calls, or compute a new result if they are different.
  */
 export function memoizeTransform(f) {
-  throw new Error('Implement the memoizeTransform function');
+  
+  let lastX = null;
+  let lastY = null;
+  let lastResult = null;
+
+  return function (x, y) {
+    if (x === lastX && y === lastY) {
+      return lastResult
+    }
+
+    lastResult = f(x,y)
+    lastX = x;
+    lastY = y;
+    return lastResult;
+  }
+
 }
